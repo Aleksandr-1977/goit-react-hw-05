82b781d90e36adebc798f67f056a105b3faed6b4
Aleksandr-1977 <vasilenkobox@gmail.com>
2025-03-18 16:13:51 +0200

    start

--- README.md
+++ README.md
@@ -1,17 +1,5 @@
 # React + Vite
 
-This template provides a minimal setup to get React working in Vite with HMR and
-some ESLint rules.
-
-Currently, two official plugins are available:
-
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md)
-  uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc)
-  uses [SWC](https://swc.rs/) for Fast Refresh
-
-Установленные библиотеки:
-
 - npm create vite@latest
 - npm i
 - npm install clsx
@@ -25,3 +13,4 @@ Currently, two official plugins are available:
 - npm install react-spinners
 - npm install react-modal
 - npm install react-hot-toast
+- npm i react-router-dom
--- src/App.jsx
+++ src/App.jsx
@@ -1,131 +1,35 @@
-import { useState, useEffect } from 'react';
-import toast, { Toaster } from 'react-hot-toast';
-import './App.css';
-import SearchBar from './components/SearchBar/SearchBar';
-import { getImages } from './apiService/getAPI';
-import ImageGallery from './components/ImageGallery/ImageGallery';
-import Loader from './components/Loader/Loader';
-import Error from './components/ErrorMessage/ErrorMessage';
-import LoadMoreBtn from './components/LoadMoreBtn/LoadMoreBtn';
-import ImageModal from './components/ImageModal/ImageModal';
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
 function App() {
-  const [query, setQuery] = useState('');
-  const [page, setPage] = useState(1);
-  const [images, setImages] = useState([]);
-  const [loading, setLoading] = useState(false);
-  const [isError, setIsError] = useState(false);
-  const [totalPages, setTotalPages] = useState(1);
+  const [count, setCount] = useState(0)
 
-  const [modalIsOpen, setIsOpen] = useState(false);
-  const [modalImage, setModalImage] = useState('');
-  const [altDescription, setAltDescription] = useState('');
-  const [currentImageIndex, setCurrentImageIndex] = useState(0);
-
-  const openModal = index => {
-    setCurrentImageIndex(index);
-    setModalImage(images[index].urls.regular);
-    setAltDescription(images[index].alt_description);
-    setIsOpen(true);
-  };
-
-  const closeModal = () => {
-    setIsOpen(false);
-  };
-
-  const updateModalStateData = index => {
-    setCurrentImageIndex(index);
-    setModalImage(images[index].urls.regular);
-    setAltDescription(images[index].alt_description);
-  };
-
-  const goToNextImage = () => {
-    if (currentImageIndex < images.length - 1) {
-      setCurrentImageIndex(currentImageIndex + 1);
-      updateModalStateData(currentImageIndex + 1);
-    }
-  };
-
-  const goToPreviousImage = () => {
-    if (currentImageIndex > 0) {
-      setCurrentImageIndex(currentImageIndex - 1);
-      updateModalStateData(currentImageIndex - 1);
-    }
-  };
-
-  useEffect(() => {
-    if (!query) return;
-
-    async function handleSearch() {
-      try {
-        setIsError(false);
-        setLoading(true);
-        const data = await getImages(query, page);
-        setTotalPages(data.total_pages);
-        if (data.total === 0) {
-          toast.error(
-            'Извините, нет результатов, соответствующих вашему поисковому запросу. Попробуйте еще раз!'
-          );
-        } else {
-          setImages(prevImages => [...prevImages, ...data.results]);
-          if (page >= data.total_pages && page > 0) {
-            toast('Сожалеем, но больше нет информации по Вашему запросу!');
-          }
-        }
-      } catch {
-        setIsError(true);
-      } finally {
-        setLoading(false);
-      }
-    }
-    handleSearch();
-  }, [query, page]);
-
-  const getQuery = image => {
-    if (image === query) return;
-    setQuery(image);
-    setImages([]);
-    setPage(1);
-  };
   return (
     <>
-      <SearchBar onSubmit={getQuery} />
-      {loading && <Loader loading={loading} />}
-      {isError && <Error />}
-      {images.length > 0 && (
-        <ImageGallery
-          images={images}
-          openModal={openModal}
-          updateModalStateData={updateModalStateData}
-        />
-      )}
-      {images.length > 0 && !loading && !isError && page < totalPages && (
-        <LoadMoreBtn
-          onClick={() => setPage(page + 1)}
-          pages={page}
-          totalPages={totalPages}
-        />
-      )}
-      <ImageModal
-        goToNextImage={goToNextImage}
-        goToPreviousImage={goToPreviousImage}
-        modalIsOpen={modalIsOpen}
-        closeModal={closeModal}
-        src={modalImage}
-        alt={altDescription}
-      />
-      <Toaster
-        position="top-right"
-        toastOptions={{
-          style: {
-            border: '1px solid rgb(45, 90, 236)',
-            padding: '16px',
-            color: '#00000',
-          },
-        }}
-      />
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
